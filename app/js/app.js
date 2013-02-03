1ea33efbca9d0467010e0bfc1b9f30a3a9577aab
Joshua Clark <joshua.clark@txstate.edu>
2013-02-03 17:15:24 -0600

    commented the directive heavily

--- app/js/app.js
+++ app/js/app.js
@@ -9,18 +9,27 @@ var TestCtrl = function($scope, Data) {
 
 app.directive('jstree', function() {
   return {
+
+    //jstree must be an attribute on an HTML element
     restrict: 'A',
+
+    //give every [jstree] its own scope
     scope: {
+      //create a two-way binding between this scope and the attribute's value
       jstree: '='
     },
+
+    //call this function to construct the element
     link: function(scope, element, attrs) {
+      //when scope.jstree.data changes, call this function
       scope.$watch('jstree.data', function() {
         $(element).jstree({
           "json_data" : scope.jstree,
           "plugins" : [ "themes", "json_data" ]
-        });
+        }, false); //false -> don't do dirty deep checking (expensive)
       });
     }
+
   };
 });
 
