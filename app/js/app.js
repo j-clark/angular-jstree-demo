2569890b5c9e2bcd1eb8c751f1f7b1f773f66bc7
jclark <jclark@mobicorp.com>
2013-02-04 13:54:08 -0600

    got require working

--- app/js/app.js
+++ app/js/app.js
@@ -1,94 +1,64 @@
 define([
   // Standard Libs
   'lib/jquery/jquery-min.js'
+  , 'lib/jstree/jquery.jstree.js'
   , 'lib/angular/angular.js'
   , 'lib/angular/angular-resource.js'
   , 'lib/angular/angular-loader.js'
   , 'lib/underscore/underscore.js'
 
-], function () {
-  //'use strict';
+  ], function () {
+    var app = angular.module('jsTreeApp', ['ngResource']);
 
-  /* App Module */
-  console.log('jQuery: ' + jQuery)
-  var app = angular.module('jsTreeApp', ['ngResource']);
+    app.factory('Data', function($resource) {
 
-  function TestCtrl($scope, Data) {
-    $scope.treeData = Data.getTreeData();
-  };
-
-  var jstreeModule = function() {
-    return {
-
-      //jstree must be an attribute on an HTML element
-      restrict: 'A',
-
-      //give every [jstree] its own scope
-      scope: {
-        //create a two-way binding between this scope and the attribute's value
-        jstree: '='
-      },
-
-      //call this function to construct the element
-      link: function(scope, element, attrs) {
-        //when scope.jstree.data changes, call this function
-        scope.$watch('jstree.data', function() {
-          $(element).jstree({
-            "json_data" : scope.jstree,
-            "plugins" : [ "themes", "json_data" ]
-          }, false); //false -> don't do dirty deep checking (expensive)
-        });
-      }
-    };
-  };
-
-  var registeredDirectives = {
-    jstree: jstreeModule
-  };
-
-  var service = function ($resource) {
-
-    return $resource('/data/treeData.json', {}, {
-      getTreeData: { method: 'GET', isArray: false }
+      return $resource('/data/treeData.json', {}, {
+        getTreeData: { method: 'GET', isArray: false }
+      });
     });
 
-  };
-  service.$inject = ['$resource'];
+    app.controller('TestCtrl', ['$scope', 'Data', function ($scope, Data) {
+      $scope.treeData = Data.getTreeData();
+    }]);
 
-  var registeredServices = {
-    Data: service
-  };  
+    app.directive('jstree', function() {
+      return {
 
+        //jstree must be an attribute on an HTML element
+        restrict: 'A',
 
-  var registeredControllers = {
-    TestCtrl: TestCtrl
-  };
-  TestCtrl.$inject = ['$scope'];
+        //give every [jstree] its own scope
+        scope: {
+          //create a two-way binding between this scope and the attribute's value
+          jstree: '='
+        },
 
-  // app.factory('Data', function($resource) {
-  //   return $resource('/data/treeData.json', {}, {
-  //     getTreeData: { method: 'GET', isArray: false }
-  //   })
-  // });
+        //call this function to construct the element
+        link: function(scope, element, attrs) {
 
-  function initialize() {
-    //initialize services
-    console.log('angular.service: ', angular.service)
-    _.extend(angular.service, registeredServices);
-    _.extend(angular.module, {jsTreeApp: app});
-    _.extend(angular.directive, registeredDirectives);
+          //when scope.jstree.data changes, call this function
+          scope.$watch('jstree', function() {
+            console.log(scope)
+            $(element).jstree({
+              "json_data" : scope.jstree,
+              "plugins" : [ "themes", "json_data" ]
+            }, false); //false -> don't do dirty deep checking (expensive)
+          });
+        }
 
-    //add controllers to angular.scope
-    var scope = angular.scope();
-    _.extend(scope, {jsTreeApp: app});
-    _.extend(scope, registeredControllers);
+      };
+    });
 
+    function initialize() {
+      //initialize services
+      console.log(angular);
+      _.extend(angular.module, {jsTreeApp: app});
 
-    //add scope to window    
-    angular.compile(window.document)(scope);
-  }
+      //add controllers to angular.scope
+    }
 
   return {
     initialize: initialize
   };
+
 });
